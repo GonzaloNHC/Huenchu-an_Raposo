07e5e2e7592e63ba9e45b854f44522b0b7f7139d
Raposito98 <88114402+Raposito98@users.noreply.github.com>
2022-04-13 10:56:11 -0400

    Add files via upload

--- Taller2/src/main/java/Taller2.java
+++ Taller2/src/main/java/Taller2.java
@@ -1,39 +1,40 @@
 public class Taller2 {
     public static void main(String[] args) {
         int a = 3;
-        double[][]sensor=new double[a][24];
+        double[][]sensor=new double[a][3];
         rellenarMatriz(sensor);
+        Mostrar(sensor);
+        sismoMayor(sensor);
 
     }
-    public static void rellenarMatriz(double[][]sensor){
+    public static double[][] rellenarMatriz(double[][]sensor){
         for (int i = 0; i < sensor.length; i++) {
-            for (int j = 0; j < 24; j++) {
-                sensor[i][j]= ((Math.random()*9.5)+1);
+            for (int j = 0; j < 3; j++) {
+                sensor[i][j]= ((Math.random()*8.5)+1);
             }
         }
-        Mostrar(sensor);
+        return sensor;
     }
     public static void Mostrar(double[][]sensor){
         for (int i = 0; i < sensor.length; i++) {
-            for (int j = 0; j < 24; j++) {
+            for (int j = 0; j < 3; j++) {
                 System.out.print(sensor[i][j]);
+                System.out.print(" ");
             }
             System.out.println();
         }
     }
-    /*public static double sismoMayor(double[][]sensor){
-        double aux;
-        for (int i = 0; i < sensor.length - 1; i++) {
-            for (int j = 0; j < sensor.length - i - 1; j++) {
-                if (sensor[j + 1] < sensor[j]) {
-                    aux = sensor[j + 1];
-                    sensor[j + 1] = sensor[j];
-                    sensor[j] = aux;
-                    return sensor;
+    public static double sismoMayor(double[][]sensor){
+        double aux=0;
+        for (int i = 0; i < sensor.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (aux < sensor[i][j]) {
+                    aux=sensor[i][j];
+                    //System.out.println(aux);
                 }
             }
         }
-
+        System.out.println(aux);
         return aux;
-    }*/
+    }
 }
